4ded778f7cc401b30b3ca1037cf383d8857ad99a
Wesley Moraes"git config --global user.name Wesley <wesleyjava88@gmail.com>
2023-03-17 21:22:23 -0300

    feat: using spring framework dependency injection container

--- backend/src/main/java/com/wminnovation/myfinances/service/impl/UsuarioServiceImpl.java
+++ backend/src/main/java/com/wminnovation/myfinances/service/impl/UsuarioServiceImpl.java
@@ -1,13 +1,18 @@
 package com.wminnovation.myfinances.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.wminnovation.myfinances.model.entity.Usuario;
 import com.wminnovation.myfinances.model.repository.UsuarioRepository;
 import com.wminnovation.myfinances.service.UsuarioService;
 
+@Service
 public class UsuarioServiceImpl implements UsuarioService {
 
 	private UsuarioRepository repository;
 
+	@Autowired
 	public UsuarioServiceImpl(UsuarioRepository repository) {
 		super();
 		this.repository = repository;
