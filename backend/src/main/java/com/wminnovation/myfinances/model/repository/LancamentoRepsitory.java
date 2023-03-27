f8389efbf80402f4be404e73ed8d1066ba5fca4b
Wesley Moraes"git config --global user.name Wesley <wesleyjava88@gmail.com>
2023-03-26 21:25:37 -0300

    feat: adding annotation bean spring framework to managent of spring

--- backend/src/main/java/com/wminnovation/myfinances/model/repository/LancamentoRepsitory.java
+++
@@ -1,9 +0,0 @@
-package com.wminnovation.myfinances.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wminnovation.myfinances.model.entity.Lancamento;
-
-public interface LancamentoRepsitory extends JpaRepository<Lancamento, Long> {
-
-}
