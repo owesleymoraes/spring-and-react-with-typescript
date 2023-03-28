959a43646a3ec1f1d0970d4be9994c1ebc98b646
Wesley Moraes"git config --global user.name Wesley <wesleyjava88@gmail.com>
2023-03-28 19:47:14 -0300

    refactor: refactor in tests of the repository

--- backend/src/test/java/com/wminnovation/myfinances/model/repository/UsuarioRepositoryTest.java
+++ backend/src/test/java/com/wminnovation/myfinances/model/repository/UsuarioRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -24,11 +25,14 @@ public class UsuarioRepositoryTest {
 	@Autowired
 	UsuarioRepository repository;
 
+	@Autowired
+	TestEntityManager entityManager;
+
 	@Test
 	public void deveVerificarAExistenciaDeUmEmail() {
 		// cenário
 		Usuario usuario = Usuario.builder().nome("usuario").email("usuario@email.com").build();
-		repository.save(usuario);
+		entityManager.persist(usuario);
 
 		// ação ou execução - aplicando o teste
 		boolean result = repository.existsByEmail("usuario@email.com");
@@ -42,7 +46,7 @@ public class UsuarioRepositoryTest {
 	public void deveRetornarFalsoQuandoNãoHouverCadastroComEmailPassado() {
 		// cenério
 
-		repository.deleteAll();
+		// repository.deleteAll();
 
 		// ação
 
