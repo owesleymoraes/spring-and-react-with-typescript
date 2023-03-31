fc42208561784c67fc613d81f4c89657abb8372f
Wesley Moraes"git config --global user.name Wesley <wesleyjava88@gmail.com>
2023-03-30 21:57:02 -0300

    feat: implemented method autenticarUsuario in package

--- backend/src/main/java/com/wminnovation/myfinances/api/resource/UsuarioResource.java
+++ backend/src/main/java/com/wminnovation/myfinances/api/resource/UsuarioResource.java
@@ -2,16 +2,16 @@ package com.wminnovation.myfinances.api.resource;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wminnovation.myfinances.api.dto.UsuarioDTO;
-import com.wminnovation.myfinances.exception.RegraNegocioException;
 import com.wminnovation.myfinances.model.entity.Usuario;
 import com.wminnovation.myfinances.service.UsuarioService;
+import com.wminnovation.myfinances.exception.ErroDeAutenticacao;
+import com.wminnovation.myfinances.exception.RegraNegocioException;
 
 @RestController
 @RequestMapping("/api/usuarios")
@@ -23,8 +23,19 @@ public class UsuarioResource {
 		this.service = service;
 	}
 
+	@PostMapping("/autenticar")
+	public ResponseEntity<?> autenticarUsuario(@RequestBody UsuarioDTO dto) {
+		try {
+			Usuario usuarioAutenticado = service.autenticar(dto.getEmail(), dto.getSenha());
+			return ResponseEntity.ok(usuarioAutenticado);
+		} catch (ErroDeAutenticacao e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+
+	}
+
 	@PostMapping
-	public ResponseEntity salvarUsuario(@RequestBody UsuarioDTO dto) {
+	public ResponseEntity<?> salvarUsuario(@RequestBody UsuarioDTO dto) {
 		Usuario usuario = Usuario.builder().nome(dto.getNome()).email(dto.getEmail()).senha(dto.getSenha()).build();
 
 		try {
