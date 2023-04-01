f4e34b5e78e3a413e990e7ef4fe85187bb587646
Wesley Moraes"git config --global user.name Wesley <wesleyjava88@gmail.com>
2023-03-31 21:45:44 -0300

    feat: signature of the methods in LancamentoServiceImpl ready

---
+++ backend/src/main/java/com/wminnovation/myfinances/service/impl/LancamentoServiceImpl.java
@@ -0,0 +1,53 @@
+package com.wminnovation.myfinances.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wminnovation.myfinances.model.entity.Lancamento;
+import com.wminnovation.myfinances.model.enuns.StatusLancamento;
+import com.wminnovation.myfinances.model.repository.LancamentoRepository;
+import com.wminnovation.myfinances.service.LancamentoService;
+
+@Service
+public class LancamentoServiceImpl implements LancamentoService {
+
+	private LancamentoRepository repository;
+
+	public LancamentoServiceImpl(LancamentoRepository repository) {
+		this.repository = repository;
+
+	}
+
+	@Override
+	public Lancamento salvarLancamento(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Lancamento atualizarLancamento(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deletarLancamento(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Lancamento> buscarLancamento(Lancamento lancamentoFiltrado) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void atualizaStatusLancemento(Lancamento lancamneto, StatusLancamento status) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
