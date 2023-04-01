0a672a8e5e9c2173f0dcfc2e4f20a85d7af0c05a
Wesley Moraes"git config --global user.name Wesley <wesleyjava88@gmail.com>
2023-03-31 22:25:23 -0300

    feat: implemented method of the buscaLancamento in the LancamentoServiceImpl

--- backend/src/main/java/com/wminnovation/myfinances/service/impl/LancamentoServiceImpl.java
+++ backend/src/main/java/com/wminnovation/myfinances/service/impl/LancamentoServiceImpl.java
@@ -3,15 +3,19 @@ package com.wminnovation.myfinances.service.impl;
 import java.util.List;
 import java.util.Objects;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.ExampleMatcher.StringMatcher;
 
 import com.wminnovation.myfinances.model.entity.Lancamento;
+import com.wminnovation.myfinances.service.LancamentoService;
 import com.wminnovation.myfinances.model.enuns.StatusLancamento;
 import com.wminnovation.myfinances.model.repository.LancamentoRepository;
-import com.wminnovation.myfinances.service.LancamentoService;
 
-import jakarta.transaction.Transactional;
+
 
 @Service
 public class LancamentoServiceImpl implements LancamentoService {
@@ -46,9 +50,11 @@ public class LancamentoServiceImpl implements LancamentoService {
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public List<Lancamento> buscarLancamento(Lancamento lancamentoFiltrado) {
-		// TODO Auto-generated method stub
-		return null;
+		Example example = Example.of(lancamentoFiltrado,
+				ExampleMatcher.matching().withIgnoreCase().withStringMatcher(StringMatcher.CONTAINING));
+		return repository.findAll(example);
 	}
 
 	@Override
