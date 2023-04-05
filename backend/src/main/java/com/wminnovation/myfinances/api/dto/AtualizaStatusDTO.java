c41c6d39b2afae3e8f8aab75aa6291f4281dbdd1
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-04-04 21:30:10 -0300

    feat: created object to update status

---
+++ backend/src/main/java/com/wminnovation/myfinances/api/dto/AtualizaStatusDTO.java
@@ -0,0 +1,12 @@
+package com.wminnovation.myfinances.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AtualizaStatusDTO {
+	private String status;
+}
