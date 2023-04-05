bf5aabf6c2b69ab94e6edbb60d37eea677a8a1a1
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-04-04 22:28:31 -0300

    feat: created method obterSaldoPeloUsuario

--- backend/src/main/java/com/wminnovation/myfinances/service/LancamentoService.java
+++ backend/src/main/java/com/wminnovation/myfinances/service/LancamentoService.java
@@ -1,5 +1,6 @@
 package com.wminnovation.myfinances.service;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,6 +8,8 @@ import com.wminnovation.myfinances.model.entity.Lancamento;
 import com.wminnovation.myfinances.model.enuns.StatusLancamento;
 
 public interface LancamentoService {
+
+	BigDecimal obterSaldoPorUsuario(Long id);
 
 	void validarLancamento(Lancamento lancamento);
 
