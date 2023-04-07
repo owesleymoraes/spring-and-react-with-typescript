337769288b3cb2592bf1da38286ace63bbf47fa4
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-04-07 16:26:01 -0300

    feat: install bootswatch

---
+++ frontEnd/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "bootswatch/dist/flatly/bootstrap.css";
+
+export const App: React.FC = () => {
+  return (
+    <>
+      <h1>MY-Wallet</h1>
+      <button type="button" className="btn btn-primary">
+        Primary
+      </button>
+    </>
+  );
+};
--- frontend/my-wallet-app/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const App: React.FC = () => {
-  return (
-    <>
-      <h1>MY-Wallet</h1>
-    </>
-  );
-};
