d4cc4304004c5a9917e69136179f7149cb5717c5
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-04-07 20:24:22 -0300

    feat: created Container component of Login page

---
+++ frontEnd/src/components/Container/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface ContainerProps {
+  children: React.ReactNode;
+}
+
+export const Container: React.FC<ContainerProps> = ({ children }) => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div
+          className="col-md-6"
+          style={{ position: "relative", left: "300px" }}
+        >
+          <div className="bs-docs-section">{children}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
