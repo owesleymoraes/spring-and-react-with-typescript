032c923f54401cb5c4ff8a7379f3304e2709e2b4
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-04-08 20:14:16 -0300

    feat: adding to test in App the CadastroUsuario components

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Login } from "./views/Login";
 import "bootswatch/dist/flatly/bootstrap.css";
+import { CadastroUsuario } from "./views/CadastroUsuario";
 
 export const App: React.FC = () => {
-  return <Login />;
+  return <CadastroUsuario />;
 };
