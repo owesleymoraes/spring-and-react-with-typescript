1c570cd3aaf94280ded65401e5816f48555cb97b
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-04-09 17:30:22 -0300

    feat: adding Navbar component in App.tsx

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { AppRoute } from "./_app-routes";
 import "bootswatch/dist/flatly/bootstrap.css";
+import { Navbar } from "./components/Navbar";
 
 export const App: React.FC = () => {
-  return <AppRoute />;
+  return (
+    <>
+      <Navbar />
+      <AppRoute />
+    </>
+  );
 };
