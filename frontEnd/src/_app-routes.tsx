eda21161358272943655f4c3aa4261811bdc5cda
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-04-09 17:29:48 -0300

    feat: adding route of the home and login page

--- frontEnd/src/_app-routes.tsx
+++ frontEnd/src/_app-routes.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Login } from "./views/Login";
 import { CadastroUsuario } from "./views/CadastroUsuario";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./views/Home";
 
 export const AppRoute: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/cadastrar" element={<CadastroUsuario />} />
       </Routes>
     </BrowserRouter>
