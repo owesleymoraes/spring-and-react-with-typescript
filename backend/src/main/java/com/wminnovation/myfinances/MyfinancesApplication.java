a6edf69ce1a28e443d5b82ac5b1f1ccd27120eee
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-04-10 22:21:15 -0300

    feat: appliced EnableWebMvc to request of frontend

--- backend/src/main/java/com/wminnovation/myfinances/MyfinancesApplication.java
+++ backend/src/main/java/com/wminnovation/myfinances/MyfinancesApplication.java
@@ -2,9 +2,20 @@ package com.wminnovation.myfinances;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class MyfinancesApplication {
+@EnableWebMvc
+public class MyfinancesApplication implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+
+//		WebMvcConfigurer.super.addCorsMappings(registry);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyfinancesApplication.class, args);
