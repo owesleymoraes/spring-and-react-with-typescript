5bc4287d6bb96a8d831558cc653180fc4dfb1941
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-04-18 21:44:40 -0300

    feat: adding route to CosultaLancamento page

--- frontEnd/src/_app-routes.tsx
+++ frontEnd/src/_app-routes.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { Home } from "./views/Home";
 import { Login } from "./views/Login";
 import { CadastroUsuario } from "./views/CadastroUsuario";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "./views/Home";
+import { ConsultaLancamento } from "./views/ConsultaLancamento";
 
 export const AppRoute: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ export const AppRoute: React.FC = () => {
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cadastrar" element={<CadastroUsuario />} />
+        <Route path="/consulta-lancamento" element={<ConsultaLancamento />} />
       </Routes>
     </BrowserRouter>
   );
