b56d1c8daf2ea2f29748ca1ee15f238848ad6978
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-04-21 15:28:21 -0300

    feat: created lancamentoTable component to lancamento page

---
+++ frontEnd/src/views/lancamentos/ConsultaLancamento/LancamentoTable/index.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { FieldRegister } from "../../../../components/FieldRegister";
+import { Button } from "../../../../components/Button";
+
+interface LancamentoTableProps {
+  lancamentos: lancamentos[];
+}
+
+interface lancamentos {
+  descricao: string;
+  valor: number;
+  tipo: string;
+  mes: number;
+  status: string;
+}
+
+export const LancamentoTable: React.FC<LancamentoTableProps> = ({
+  lancamentos,
+}) => {
+  return (
+    <FieldRegister widthField={12} typeColumn="md">
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">Descrição</th>
+            <th scope="col">Valor</th>
+            <th scope="col">Tipo</th>
+            <th scope="col">Mês</th>
+            <th scope="col">Situação</th>
+            <th scope="col">Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {lancamentos.map((item, index) => {
+            return (
+              <tr key={index}>
+                <th scope="row">{item.descricao}</th>
+                <td>{item.valor}</td>
+                <td>{item.tipo}</td>
+                <td>{item.mes}</td>
+                <td>{item.status}</td>
+                <td>
+                  <Button
+                    title="Editar"
+                    typeButton="primary"
+                    onClick={() => {}}
+                  />
+                  <Button
+                    title="Deletar"
+                    typeButton="danger"
+                    onClick={() => {}}
+                  />
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </FieldRegister>
+  );
+};
