39b66eb2111edd1a9d5a4dff4d046b182afecc1e
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-04-24 20:51:15 -0300

    feat: install of lib primereact to use modal and icons in project

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
-import "toastr/build/toastr.css"
+import "toastr/build/toastr.css";
 import { AppRoute } from "./_app-routes";
 import { Navbar } from "./components/Navbar";
 import "bootswatch/dist/flatly/bootstrap.css";
+//core
+import "primereact/resources/primereact.min.css";
+//theme
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import { Button } from "primereact/button";
 
 export const App: React.FC = () => {
   return (
