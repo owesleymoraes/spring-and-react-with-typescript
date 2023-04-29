f5777e92cb5c36e1c802776e60d1decf550a2bcb
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-04-29 19:35:25 -0300

    feat: created folder of util with two files

---
+++ frontEnd/src/_utils/typesRelease.ts
@@ -0,0 +1,7 @@
+import { OptionsSelect } from "../components/Select";
+
+export const releaseTypes: OptionsSelect[] = [
+  { value: "", label: "Selecione..." },
+  { value: "DESPESA", label: "Despesa" },
+  { value: "RECEITA", label: "Receita" },
+];
