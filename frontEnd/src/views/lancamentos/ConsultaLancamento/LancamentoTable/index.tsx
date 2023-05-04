39357a5a2f981467b46007822010c23505f62f04
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-05-03 23:14:34 -0300

    feat: created method of alter status in fetch release

--- frontEnd/src/views/lancamentos/ConsultaLancamento/LancamentoTable/index.tsx
+++ frontEnd/src/views/lancamentos/ConsultaLancamento/LancamentoTable/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useEffect} from "react";
 import { Button } from "../../../../components/Button";
 import { FieldRegister } from "../../../../components/FieldRegister";
 
@@ -6,6 +6,7 @@ interface LancamentoTableProps {
   releases: lancamentosResponse[];
   onEditRelease: (id: number) => void;
   onDeleteRelease: (release: lancamentosResponse) => void;
+  onAlterStatus: (release: lancamentosResponse, status: string) => void;
 }
 
 export interface lancamentosResponse {
@@ -20,6 +21,7 @@ export interface lancamentosResponse {
 export const LancamentoTable: React.FC<LancamentoTableProps> = ({
   releases,
   onEditRelease,
+  onAlterStatus,
   onDeleteRelease,
 }) => {
   const formatterMoney = new Intl.NumberFormat("pt-BR", {
@@ -27,6 +29,7 @@ export const LancamentoTable: React.FC<LancamentoTableProps> = ({
     currency: "BRL",
   });
 
+  
   return (
     <FieldRegister widthField={12} typeColumn="md">
       <table className="table table-hover">
@@ -50,6 +53,21 @@ export const LancamentoTable: React.FC<LancamentoTableProps> = ({
                 <td>{item.mes}</td>
                 <td>{item.status}</td>
                 <td>
+                  <Button
+                    title="Efetivar"
+                    typeButton="success"
+                    onClick={() => {
+                      onAlterStatus(item, "EFETIVADO");
+                    }}
+                  />
+
+                  <Button
+                    title="Cancelar"
+                    typeButton="warning"
+                    onClick={() => {
+                      onAlterStatus(item, "CANCELADO");
+                    }}
+                  />
                   <Button
                     title="Editar"
                     typeButton="primary"
