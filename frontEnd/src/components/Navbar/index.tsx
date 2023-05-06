1188fb098ab6c604030720326fac68e86d983e25
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-05-06 18:40:33 -0300

    feat: remove code empty

--- frontEnd/src/components/Navbar/index.tsx
+++ frontEnd/src/components/Navbar/index.tsx
@@ -12,17 +12,6 @@ export const Navbar: React.FC = () => {
         >
           Minhas Finanças
         </a>
-        {/* <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarResponsive"
-          aria-controls="navbarResponsive"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button> */}
         <ButtonNavbar />
       </div>
     </div>
