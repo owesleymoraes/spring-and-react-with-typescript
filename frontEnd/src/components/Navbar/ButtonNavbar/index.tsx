f0cb8d32d00938cdc3a2582fc1a4b97456db95df
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-05-06 18:41:28 -0300

    feat: adding method to exit of login and conditional in navbar

--- frontEnd/src/components/Navbar/ButtonNavbar/index.tsx
+++ frontEnd/src/components/Navbar/ButtonNavbar/index.tsx
@@ -1,27 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../../../_context";
+import { AuthService } from "../../../_app/service/authService";
 
 export const ButtonNavbar = () => {
+  const { userIsLogged, setUserIsLogged } = useContext(AuthContext);
+
+  const handleClickExit = () => {
+    AuthService.removeUserAuthenticated();
+     setUserIsLogged(false);
+  };
+  
   return (
     <div className="collapse navbar-collapse" id="navbarResponsive">
       <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link" href="/home">
-            Home
-          </a>
-        </li>
+        {userIsLogged && (
+          <li className="nav-item">
+            <a className="nav-link" href="/home">
+              Home
+            </a>
+          </li>
+        )}
         <li className="nav-item">
           <a className="nav-link" href="/cadastrar">
             Cadastro
           </a>
         </li>
+        {userIsLogged && (
+          <li className="nav-item">
+            <a className="nav-link" href="/consulta-lancamento">
+              Lançamentos
+            </a>
+          </li>
+        )}
         <li className="nav-item">
-          <a className="nav-link" href="/consulta-lancamento">
-            Lançamentos
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/login">
-            Login
+          <a onClick={() => handleClickExit()} className="nav-link" href="/">
+            Sair
           </a>
         </li>
       </ul>
