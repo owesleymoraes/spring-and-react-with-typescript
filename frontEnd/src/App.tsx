ddece87d5e546aaf757a1b9a83830b8ae86d0b10
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-05-06 18:38:16 -0300

    feat: provide the context api in root project

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -9,12 +9,13 @@ import "primereact/resources/primereact.min.css";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 //icons
 import "primeicons/primeicons.css";
+import { AuthContextProvider } from "./_context";
 
 export const App: React.FC = () => {
   return (
-    <>
+    <AuthContextProvider>
       <Navbar />
       <AppRoute />
-    </>
+    </AuthContextProvider>
   );
 };
