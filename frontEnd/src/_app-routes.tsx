5bca6f3163968a2bb96fffaf7feaddefadfbad5b
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-05-05 23:41:40 -0300

    feat: implemeted limit of access the route only logged

--- frontEnd/src/_app-routes.tsx
+++ frontEnd/src/_app-routes.tsx
@@ -1,24 +1,28 @@
 import React from "react";
 import { Home } from "./views/Home";
 import { Login } from "./views/Login";
+import { AuthProvider, useAuth } from "./_context";
 import { CadastroUsuario } from "./views/CadastroUsuario";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ConsultaLancamento } from "./views/lancamentos/ConsultaLancamento";
 import { CadastroDeLancamento } from "./views/lancamentos/CadastroLancamento";
 
+
 export const AppRoute: React.FC = () => {
+  const { isAuthenticated } = useAuth();
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/cadastrar" element={<CadastroUsuario />} />
-        <Route path="/consulta-lancamento" element={<ConsultaLancamento />} />
-        <Route
-          path="/cadastro-lancamento/:idReleaseEdit?"
-          element={<CadastroDeLancamento />}
-        />
-      </Routes>
+      <AuthProvider>
+        <Routes>      
+          <Route path="/" element={<Login />} />
+          <Route path="/cadastrar" element={<CadastroUsuario />} />
+
+          <Route path="/home" element={isAuthenticated ? <Home /> : <Navigate to="/" replace />}/>
+          <Route path="/consulta-lancamento" element={isAuthenticated ? <ConsultaLancamento /> : <Navigate to="/" replace />}/>
+          <Route path="/cadastro-lancamento/:idReleaseEdit?" element={isAuthenticated ? <CadastroDeLancamento /> : <Navigate to="/" replace />}/>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 };
