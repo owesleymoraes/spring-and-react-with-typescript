a4bfa35a0787330345ecff1bb1bac2f253090f12
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-05-09 21:12:24 -0300

    feat: remove code empty

--- frontEnd/src/components/Card/index.tsx
+++ frontEnd/src/components/Card/index.tsx
@@ -10,14 +10,9 @@ export const Card: React.FC<CardProps> = ({ title, children }) => {
     <div className="card md-3">
       <h3 className="card-header">{title}</h3>
       <div className="card-body">
-        {/* <div className="row">
-          <div className="col-lg-12">
-            <div className="bs-component"> */}
         {children}
       </div>
     </div>
-    //     </div>
-    //   </div>
-    // </div>
+   
   );
 };
