37fe5e457db23a5641a13496489a44501aee96a6
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-05-10 22:30:59 -0300

    feat: aplication of stateless in requests

--- backend/src/main/java/com/wminnovation/myfinances/config/SecurityConfiguration.java
+++ backend/src/main/java/com/wminnovation/myfinances/config/SecurityConfiguration.java
@@ -1,23 +1,24 @@
 package com.wminnovation.myfinances.config;
 
-
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-	
+
 	@Bean
 	public UserDetailsService useDetailsService(PasswordEncoder encode) {
 		UserDetails user = User
@@ -34,19 +35,22 @@ public class SecurityConfiguration {
 		return new BCryptPasswordEncoder();
 	}
 
-	
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		return http
-				.csrf().disable()
-				.authorizeHttpRequests()
-				.requestMatchers("").permitAll()
-				.and()
-				.authorizeHttpRequests().requestMatchers("/")
+		return http.csrf().disable().authorizeHttpRequests()
+				.requestMatchers(HttpMethod.POST, "/api/usuarios/autenticar")
+				.permitAll()
+				.requestMatchers(HttpMethod.POST, "/api/usuarios")
+				.permitAll()
+				.anyRequest()
 				.authenticated()
 				.and()
-				.formLogin()
+				.sessionManagement().sessionCreationPolicy((SessionCreationPolicy.STATELESS))
+				.and()
+				.httpBasic()
 				.and()
 				.build();
 	}
+
 }
