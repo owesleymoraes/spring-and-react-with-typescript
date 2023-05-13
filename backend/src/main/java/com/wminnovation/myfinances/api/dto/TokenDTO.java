a1269f5973c13cbcecc03a960921678a3940db22
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-05-13 17:55:23 -0300

    feat: created the class TokenDTO

--- backend/src/main/java/com/wminnovation/myfinances/api/dto/TokenDTO.java
+++ backend/src/main/java/com/wminnovation/myfinances/api/dto/TokenDTO.java
@@ -1,5 +1,16 @@
 package com.wminnovation.myfinances.api.dto;
 
+import lombok.Data;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TokenDTO {
+	private String nome;
+	private String token;
 
 }
