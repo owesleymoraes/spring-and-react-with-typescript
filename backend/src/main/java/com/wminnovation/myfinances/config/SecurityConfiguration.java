8c7889f770473a217ed8dd1a9c0343029d724a19
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-05-19 21:52:53 -0300

    feat: adding addFilterBefore

--- backend/src/main/java/com/wminnovation/myfinances/config/SecurityConfiguration.java
+++ backend/src/main/java/com/wminnovation/myfinances/config/SecurityConfiguration.java
@@ -1,8 +1,15 @@
 package com.wminnovation.myfinances.config;
 
 import org.springframework.http.HttpMethod;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.wminnovation.myfinances.service.FilterToken;
+
+import jakarta.servlet.Filter;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,39 +19,32 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-	
-	
+
+	@Autowired
+	private FilterToken filter;
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
-	@Bean
-	public AuthenticationManager authenticationManager (AuthenticationConfiguration authenticationConfiguration) throws Exception  {
-		return authenticationConfiguration.getAuthenticationManager();
-		
-	}
 
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+			throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+
+	}
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		return http.csrf().disable().authorizeHttpRequests()
-				.and()
-				.authorizeHttpRequests()
-				.requestMatchers(HttpMethod.POST, "/api/usuarios/autenticar")
-				.permitAll()
-				.requestMatchers(HttpMethod.POST, "/api/usuarios")
-				.permitAll()
-				.anyRequest().authenticated()
-				.and()
-				.sessionManagement().sessionCreationPolicy((SessionCreationPolicy.STATELESS))
-				.and()
-				.build();
+		return http.csrf().disable().authorizeHttpRequests().and().authorizeHttpRequests()
+				.requestMatchers(HttpMethod.POST, "/api/usuarios/autenticar").permitAll()
+				.requestMatchers(HttpMethod.POST, "/api/usuarios").permitAll().anyRequest().authenticated().and()
+				.sessionManagement().sessionCreationPolicy((SessionCreationPolicy.STATELESS)).and()
+				.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class).build();
 	}
 
 }
