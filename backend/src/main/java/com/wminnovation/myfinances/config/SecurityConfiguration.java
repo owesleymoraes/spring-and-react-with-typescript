03a78d777915762771c689775acf85df587d592e
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-05-20 23:27:41 -0300

    feat: clean code

--- backend/src/main/java/com/wminnovation/myfinances/config/SecurityConfiguration.java
+++ backend/src/main/java/com/wminnovation/myfinances/config/SecurityConfiguration.java
@@ -16,6 +16,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
@@ -42,8 +43,11 @@ public class SecurityConfiguration {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		return http.csrf().disable().authorizeHttpRequests().and().authorizeHttpRequests()
 				.requestMatchers(HttpMethod.POST, "/api/usuarios/autenticar").permitAll()
-				.requestMatchers(HttpMethod.POST, "/api/usuarios").permitAll().anyRequest().authenticated().and()
-				.sessionManagement().sessionCreationPolicy((SessionCreationPolicy.STATELESS)).and()
+				.requestMatchers(HttpMethod.POST, "/api/usuarios").permitAll()
+				.anyRequest().authenticated()
+				.and()
+				.sessionManagement().sessionCreationPolicy((SessionCreationPolicy.STATELESS))
+				.and()
 				.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class).build();
 	}
 
