7213e427b05627c7a85cfb2aef949ec809bad8fe
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-05-20 23:28:26 -0300

    feat: adding try catch

--- backend/src/main/java/com/wminnovation/myfinances/service/impl/TokenServiceImpl.java
+++ backend/src/main/java/com/wminnovation/myfinances/service/impl/TokenServiceImpl.java
@@ -5,11 +5,12 @@ import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.Date;
 
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.wminnovation.myfinances.model.entity.Usuario;
 import com.wminnovation.myfinances.service.TokenService;
 
@@ -21,18 +22,36 @@ public class TokenServiceImpl implements TokenService {
 
 	@Override
 	public String gerarToken(Usuario usuario) {
-		return JWT.create().withIssuer("Produtos").withSubject(usuario.getEmail()).withClaim("nome", usuario.getNome())
-				.withExpiresAt(Date.from(expiresAt)).sign(Algorithm.HMAC256("secreta"));
+		try {
+			return JWT.create().withIssuer("Produtos")
+					.withSubject(usuario.getEmail())
+					.withClaim("nome", usuario.getNome())
+					.withExpiresAt(Date.from(expiresAt))
+					.sign(Algorithm.HMAC256("secreta"));
+			
+		}  catch (JWTVerificationException ex) {
+	        
+	        throw new RuntimeException("Falha na geração  do token JWT: " + ex.getMessage(), ex);
+	    }
+		
 
 	}
 	
 
 	@Override
 	public String getSubject(String token) {
+		try {
+
+			return JWT.require(Algorithm.HMAC256("secreta"))
+					.withIssuer("Produtos")
+					.build().verify(token).getSubject();
+			
+		}  catch (JWTVerificationException ex) {
+			
+	        
+	        throw new RuntimeException("Falha na verificação do token JWT: " + ex.getMessage(), ex);
+	    }
 		
-		return JWT.require(Algorithm.HMAC256("secreta"))
-				.withIssuer("Produtos")
-				.build().verify(token).getSubject();
 		
 	}
 
