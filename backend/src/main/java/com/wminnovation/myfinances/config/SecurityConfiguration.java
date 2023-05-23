31d461543c2a07d8110a9a3daae3a71282e37389
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-05-22 23:15:24 -0300

    feat: adding filter cors to request frontend

--- backend/src/main/java/com/wminnovation/myfinances/config/SecurityConfiguration.java
+++ backend/src/main/java/com/wminnovation/myfinances/config/SecurityConfiguration.java
@@ -1,16 +1,25 @@
 package com.wminnovation.myfinances.config;
 
 import org.springframework.http.HttpMethod;
+
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.wminnovation.myfinances.service.FilterToken;
 
 import jakarta.servlet.Filter;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -49,6 +58,28 @@ public class SecurityConfiguration {
 				.sessionManagement().sessionCreationPolicy((SessionCreationPolicy.STATELESS))
 				.and()
 				.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class).build();
+	}
+	
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFiler() {
+		List<String> all = Arrays.asList("*");
+		 CorsConfiguration corsConfiguration = new  CorsConfiguration();
+		 corsConfiguration.setAllowedMethods(all);
+		 corsConfiguration.setAllowedOriginPatterns(all);
+		 corsConfiguration.setAllowedHeaders(all);
+		 corsConfiguration.setAllowCredentials(true);
+		 
+		 UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		 source.registerCorsConfiguration("/**", corsConfiguration);
+		 
+		 CorsFilter corsFilter = new CorsFilter(source);
+		  FilterRegistrationBean<CorsFilter> filterRegistrationBean = new FilterRegistrationBean<CorsFilter>(corsFilter);
+		  filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		  
+		  return filterRegistrationBean;
+		  
+		  
+		 
 	}
 
 }
