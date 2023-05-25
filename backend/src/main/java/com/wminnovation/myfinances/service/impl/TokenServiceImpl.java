4c03b59c9f9e18b06244d11b9cf7b5a29847a1d5
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-05-24 23:05:25 -0300

    feat: adding a claims of userid

--- backend/src/main/java/com/wminnovation/myfinances/service/impl/TokenServiceImpl.java
+++ backend/src/main/java/com/wminnovation/myfinances/service/impl/TokenServiceImpl.java
@@ -3,8 +3,8 @@ package com.wminnovation.myfinances.service.impl;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
-
 
 import org.springframework.stereotype.Service;
 
@@ -17,42 +17,38 @@ import com.wminnovation.myfinances.service.TokenService;
 @Service
 public class TokenServiceImpl implements TokenService {
 
-	LocalDateTime now = LocalDateTime.now().plusMinutes(90);
+	LocalDateTime now = LocalDateTime.now().plusMinutes(180);
 	Instant expiresAt = now.atOffset(ZoneOffset.of("-03:00")).toInstant();
+	String horaExpiracaoToken = now.toLocalTime().format(DateTimeFormatter.ofPattern("HH:mm"));
 
 	@Override
 	public String gerarToken(Usuario usuario) {
 		try {
-			return JWT.create().withIssuer("Produtos")
-					.withSubject(usuario.getEmail())
+			return JWT.create().withIssuer("Login").withSubject(usuario.getEmail())
 					.withClaim("nome", usuario.getNome())
+					.withClaim("horaExpiracaoToken", horaExpiracaoToken)
+					.withClaim("userid", usuario.getId())
 					.withExpiresAt(Date.from(expiresAt))
 					.sign(Algorithm.HMAC256("secreta"));
-			
-		}  catch (JWTVerificationException ex) {
-	        
-	        throw new RuntimeException("Falha na geração  do token JWT: " + ex.getMessage(), ex);
-	    }
-		
+
+		} catch (JWTVerificationException ex) {
+
+			throw new RuntimeException("Falha na geração  do token JWT: " + ex.getMessage(), ex);
+		}
 
 	}
-	
 
 	@Override
 	public String getSubject(String token) {
 		try {
 
-			return JWT.require(Algorithm.HMAC256("secreta"))
-					.withIssuer("Produtos")
-					.build().verify(token).getSubject();
-			
-		}  catch (JWTVerificationException ex) {
-			
-	        
-	        throw new RuntimeException("Falha na verificação do token JWT: " + ex.getMessage(), ex);
-	    }
-		
-		
+			return JWT.require(Algorithm.HMAC256("secreta")).withIssuer("Login").build().verify(token).getSubject();
+
+		} catch (JWTVerificationException ex) {
+
+			throw new RuntimeException("Falha na verificação do token JWT: " + ex.getMessage(), ex);
+		}
+
 	}
 
 }
